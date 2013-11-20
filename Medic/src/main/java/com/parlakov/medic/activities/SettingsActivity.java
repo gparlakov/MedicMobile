861e286d8a001575d2f4d621c05f817b99b599d4
gparlakov <gparlakov@gmail.com>
2013-11-20 15:23:54 +0200

    Should be shown on master

--- Medic/src/main/java/com/parlakov/medic/activities/SettingsActivity.java
+++ Medic/src/main/java/com/parlakov/medic/activities/SettingsActivity.java
@@ -97,7 +97,6 @@ public class SettingsActivity extends FragmentActivity{
                         .addToBackStack("registerFragment")
                         .commit();
             }
-            //change
         });
     }
 
